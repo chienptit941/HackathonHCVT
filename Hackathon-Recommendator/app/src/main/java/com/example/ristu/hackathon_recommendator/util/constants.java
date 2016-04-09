7bf2b5d26dfd95234546bd4e4febc068f6deaa8d
riskvip <vinhresearch@gmail.com>
2016-04-10 00:13:52 +0700

    aaaa

---
+++ Hackathon-Recommendator/app/src/main/java/com/example/ristu/hackathon_recommendator/util/constants.java
@@ -0,0 +1,9 @@
+package com.example.ristu.hackathon_recommendator.util;
+
+/**
+ * Created by ristu on 4/9/2016.
+ */
+public class Constants {
+    public static final String IP = "192.168.1.141";
+
+}
