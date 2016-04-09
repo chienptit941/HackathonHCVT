2f6567879ffd2a372ef2204b92bb02ad598d70ce
riskvip <vinhresearch@gmail.com>
2016-04-09 11:44:34 +0700

    update user activity

--- Hackathon-Recommendator/app/src/main/java/com/example/ristu/hackathon_recommendator/UserActivity.java
+++ Hackathon-Recommendator/app/src/main/java/com/example/ristu/hackathon_recommendator/UserActivity.java
@@ -14,7 +14,7 @@ public class UserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        setContentView(R.layout.user_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
