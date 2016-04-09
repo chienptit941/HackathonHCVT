a2ffa5d84ea00a5c7169af814bed140c242fc10b
riskvip <vinhresearch@gmail.com>
2016-04-09 13:29:42 +0700

    user activity

--- Hackathon-Recommendator/app/src/main/java/com/example/ristu/hackathon_recommendator/user/UserActivity.java
+++ Hackathon-Recommendator/app/src/main/java/com/example/ristu/hackathon_recommendator/user/UserActivity.java
@@ -2,13 +2,20 @@ package com.example.ristu.hackathon_recommendator.user;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.ristu.hackathon_recommendator.R;
+import com.example.ristu.hackathon_recommendator.model.SubjectDTO;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements IUserActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,17 @@ public class UserActivity extends AppCompatActivity {
         setContentView(R.layout.user_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new UserAdapter({"aaadsd a", "sdsdsdsds"});
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 
     @Override
@@ -32,5 +50,25 @@ public class UserActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void showSubjectDetail(SubjectDTO subjectDTO) {
+        // Show subject detail
+    }
+
+    @Override
+    public void showRate(SubjectDTO subjectDTO) {
+        // Show rate
+    }
+
+    @Override
+    public void rate(SubjectDTO subjectDTO) {
+        // button
+    }
+
+    @Override
+    public void register(SubjectDTO subjectDTO) {
+        // button
     }
 }
--- Hackathon-Recommendator/app/src/main/java/com/example/ristu/hackathon_recommendator/user/UserAdapter.java
+++ Hackathon-Recommendator/app/src/main/java/com/example/ristu/hackathon_recommendator/user/UserAdapter.java
@@ -11,14 +11,11 @@ import com.example.ristu.hackathon_recommendator.R;
 /**
  * Created by ristu on 4/9/2016.
  */
-public class UserAdapter {
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     private String[] mDataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
         public TextView mTextView;
         public ViewHolder(TextView v) {
             super(v);
@@ -26,29 +23,21 @@ public class UserAdapter {
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public UserAdapter(String[] myDataset) {
         mDataset = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public UserAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
-        // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.user_item, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-//        ...
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         holder.mTextView.setText(mDataset[position]);
 
     }
