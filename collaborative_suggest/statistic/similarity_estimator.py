dd4c81576d527775884d67f38e26add3cba820df
hact <hacaothanh@admicro.vn>
2016-04-09 10:59:55 +0700

    upload similarity

---
+++ collaborative_suggest/statistic/similarity_estimator.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def item_cosine_estimate(item_vector_a, item_vector_b):
+    dot_product = np.dot(item_vector_a, item_vector_b)
+    item_vector_a_mod = np.linalg.norm(item_vector_a)
+    item_vector_b_mod = np.linalg.norm(item_vector_b)
+    cosine_similarity = dot_product / (item_vector_a_mod * item_vector_b_mod)
+    return cosine_similarity
+
+
+def pearsonr(x, y):
+    n = len(x)
+    sum_x = float(sum(x))
+    sum_y = float(sum(y))
+    sum_x_sq = sum(map(lambda x: pow(x, 2), x))
+    sum_y_sq = sum(map(lambda y: pow(y, 2), y))
+    p_sum = sum(map(lambda x, y: x * y, x, y))
+    num = p_sum - (sum_x_sq * sum_y_sq / n)
+    den = pow((sum_x_sq - pow(sum_x, 2) / n) * (sum_y_sq - pow(sum_y, 2) / n), 0.5)
+    if den == 0:
+        return 0
+    return num / den
+
+
+def average(x):
+    n = len(x)
+    sum_x = float(sum(x))
+    if n == 0:
+        return 0
+    return sum_x / n
+
+
+
